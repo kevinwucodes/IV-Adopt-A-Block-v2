6c4b41ee7e05e8ee8d4f5382d17883ebbb9a7db1
De-Lac <daniele.fani@unicam.it>
2014-07-29 19:28:49 -0700

    contains function to code/decode JSON

---
+++ objectToJson.js
@@ -0,0 +1,16 @@
+ /** points = LatLng[]
+	  name = name of the block (label)
+  */
+ function polygonToJSON(points, name)
+ {
+  var json = '{"polygon": {"name":"'+name+'", "coordinates": [';
+  for (i=0; i<points.length; i++)
+   {
+    point = points[i];
+	json += ' {"lat":"'+point.lat+'", "lng":"'+point.lng+'"}';   
+	if (i < points.length-1)
+	   {json += ',';}  
+   }
+  json += ']}}';	 
+  return json;
+ }      
