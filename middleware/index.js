5b27a5a086c0998a8711f9492fc7a5b71493aa53
Kevin Wu <kevin.wu@gmail.com>
2014-08-03 07:13:45 +0000

    finalized POST /users route.  If user already exist, we need to just add a new tripID. If not, we create a new user, then add the new tripID

--- middleware/index.js
+++ middleware/index.js
@@ -8,6 +8,7 @@ var NODEPORT = process.env.NITROUS_PORT || process.env.OPENSHIFT_PORT || 3000;
 var restify = require('restify');
 var mongojs = require('mongojs');
 var uuid = require('node-uuid');
+var _ = require('underscore');
 
 // collections config
 var db = mongojs(MONGO_URI);
@@ -32,39 +33,72 @@ server.post('/users', function (req, res, next) {
     "tripCategory":"individual"
   }
   */
+  
+  var now = new Date().getTime();  
 
   // uuid v4 generation
   var uniqueid = uuid.v4();
 
   var payload = {
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
+    name: {
+      firstname: req.body.firstname,
+      lastname: req.body.lastname
+    },
     tripCategory: req.body.tripCategory,
-    uuid: uniqueid
+    uuid: uniqueid    
   }
+  
+  // if we have the user, append a new trip.  If not, we need to create a new user
+  db.collection('users').findAndModify({
+    query: payload.name,
+    update: {
 
-  console.log(payload);
-
-  // attempt to save to DB
-  db.collection('users').save(payload, function(err, doc){
-    // lets confirm that everything was truly saved by validating the doc
-    if (   doc.firstname === payload.firstname
-        && doc.lastname === payload.lastname
-        && doc.tripCategory === payload.tripCategory) {
-      // yup, lets tell the user it's a-ok
-      console.log("yup, it's all the same");
-      console.log(doc.uuid);
-      console.log(payload.uuid);
-      console.log(doc._id);
+       // the problem with this is that it updates the created every time, not sure how to turn this off
+//       $set: {
+//         created: new Date().getTime()
+//       },      
       
-      res.send(201, {status:"created", code: 201});
+      // add new tripID to trips array
+      $push: {
+        trips: {
+          tripID: payload.uuid,
+          created: now,
+          tripCategory: payload.tripCategory
+        }
+      }      
+    },
+
+    // returns the modified document instead of the original
+    new: true,
+    
+    // if it doesn't find, then create a new object
+    upsert: true
+  }, function(err, doc, lastErrorObject) {
+    
+    if (err) { return next(err); }
+    
+    // confirm that everything was truly saved by validating the doc object
+        
+    // verify that we have the new tripID inserted
+    var wasTripInserted = _.where(doc.trips, {
+      tripID: payload.uuid,
+      created: now,
+      tripCategory: payload.tripCategory
+    });
+        
+    // was everything saved?
+    if (   doc.firstname === payload.name.firstname
+        && doc.lastname === payload.name.lastname
+        && wasTripInserted.length) {
+      // yup, lets tell the user it's a-ok    
+      res.send(201, { UUID: wasTripInserted[0].tripID });
     }
     else {
       // uh oh, we couldn't save this in the db
       res.send(500, "something went horribly wrong");
-    }
+    }    
+  });
 
-  }); //end db.collection
   return next();
 });
 
