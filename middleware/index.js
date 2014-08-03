6f9e14cfead5e165d426b2c987010dcb172178aa
Kevin Wu <kevin.wu@gmail.com>
2014-08-03 07:26:59 +0000

    jslint and whitespace edits

--- middleware/index.js
+++ middleware/index.js
@@ -23,7 +23,7 @@ server.use(restify.bodyParser());
 // POST request
 //
 
-server.post('/users', function (req, res, next) {
+server.post('/users', function(req, res, next) {
 
   /*
   expecting:
@@ -33,8 +33,8 @@ server.post('/users', function (req, res, next) {
     "tripCategory":"individual"
   }
   */
-  
-  var now = new Date().getTime();  
+
+  var now = new Date().getTime();
 
   // uuid v4 generation
   var uniqueid = uuid.v4();
@@ -45,19 +45,19 @@ server.post('/users', function (req, res, next) {
       lastname: req.body.lastname
     },
     tripCategory: req.body.tripCategory,
-    uuid: uniqueid    
+    uuid: uniqueid
   }
-  
+
   // if we have the user, append a new trip.  If not, we need to create a new user
   db.collection('users').findAndModify({
     query: payload.name,
     update: {
 
-       // the problem with this is that it updates the created every time, not sure how to turn this off
-//       $set: {
-//         created: new Date().getTime()
-//       },      
-      
+      // the problem with this is that it updates the created every time, not sure how to turn this off
+      //       $set: {
+      //         created: new Date().getTime()
+      //       },      
+
       // add new tripID to trips array
       $push: {
         trips: {
@@ -65,38 +65,39 @@ server.post('/users', function (req, res, next) {
           created: now,
           tripCategory: payload.tripCategory
         }
-      }      
+      }
     },
 
     // returns the modified document instead of the original
     new: true,
-    
+
     // if it doesn't find, then create a new object
     upsert: true
   }, function(err, doc, lastErrorObject) {
-    
-    if (err) { return next(err); }
-    
+
+    if (err) {
+      return next(err);
+    }
+
     // confirm that everything was truly saved by validating the doc object
-        
+
     // verify that we have the new tripID inserted
     var wasTripInserted = _.where(doc.trips, {
       tripID: payload.uuid,
       created: now,
       tripCategory: payload.tripCategory
     });
-        
+
     // was everything saved?
-    if (   doc.firstname === payload.name.firstname
-        && doc.lastname === payload.name.lastname
-        && wasTripInserted.length) {
+    if (doc.firstname === payload.name.firstname && doc.lastname === payload.name.lastname && wasTripInserted.length) {
       // yup, lets tell the user it's a-ok    
-      res.send(201, { UUID: wasTripInserted[0].tripID });
-    }
-    else {
+      res.send(201, {
+        UUID: wasTripInserted[0].tripID
+      });
+    } else {
       // uh oh, we couldn't save this in the db
       res.send(500, "something went horribly wrong");
-    }    
+    }
   });
 
   return next();
@@ -108,11 +109,11 @@ server.post('/users', function (req, res, next) {
 //
 
 
-server.get('/', function (req, res, next){
-  db.collection('users').find(function(err, docs){
+server.get('/', function(req, res, next) {
+  db.collection('users').find(function(err, docs) {
     console.log(docs);
   });
-q
+  q
   res.send(200, "get");
   return next();
 });
